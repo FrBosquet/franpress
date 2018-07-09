88cd8ab55b7b4e1245a0b39791bcd4f81dd7a913
Fran Bosquet <fran@redradix.com>
2018-07-09 19:08:42 +0200

    Add heading content wrapper shadows and borders

--- src/ui/atoms/content-heading-wrapper/index.js
+++ src/ui/atoms/content-heading-wrapper/index.js
@@ -8,10 +8,13 @@ const ContentHeadingWrapper = styled.div`
 	height: 490px;
 	background: linear-gradient(
 		to bottom,
-		rgba(235, 235, 235, 0.2) 0%,
+		rgba(235, 235, 235, 0.2) 10%,
 		rgba(235, 235, 235, 1) 100%
 	);
 	padding: 20px;
+	box-shadow: 0 -5px 5px rgba(0, 0, 0, 0.65);
+	border-left: 2px solid rgba(146, 26, 56, 0.2);
+	border-right: 2px solid rgba(146, 26, 56, 0.2);
 
 	& > h1 {
 		color: #931a38;

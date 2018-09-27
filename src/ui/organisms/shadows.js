90ce30141b7cde4e24f23fceba1322cd8373b679
Fran Bosquet <fran@redradix.com>
2018-09-27 09:27:59 +0200

    Add drop shadows over bg image

---
+++ src/ui/organisms/shadows.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ShadowsLayout = styled.div`
+  position: absolute;
+  top: 560px;
+  left: 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`
+const Shadow = styled.div`
+  width: calc(( 100% - 960px ) / 2);
+  height: 5px;
+  background: none;
+  box-shadow: 0 -5px 5px rgba(0,0,0,0.65);
+`
+
+export const Shadows = () =>
+	<ShadowsLayout>
+		<Shadow />
+		<Shadow />
+	</ShadowsLayout>
--- src/ui/templates/blog.js
+++ src/ui/templates/blog.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import { Content, Header, Navigation, Interaction } from '../organisms'
 import { Copy } from '../organisms/copy'
+import { Shadows } from '../organisms/shadows'
 
 const Blog = () => (
 	<div>
 		<Header />
+		<Shadows />
 		<Content />
 		<Navigation />
 		<Interaction />

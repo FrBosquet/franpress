83371801093b1776e8c19f69de6b3ccd49431899
Fran Bosquet <fran@redradix.com>
2018-10-04 21:39:51 +0200

    :white-check-mark::construction: Some progress in the text parser

--- src/utils/texparser.spec.js
+++ src/utils/texparser.spec.js
@@ -1,4 +1,4 @@
-import { getTree, get, set } from './textparser'
+import { getTree, get, set, goUp, goRight, goLeft } from './textparser'
 
 const tree = [
 	{
@@ -61,18 +61,77 @@ describe('Getter', () => {
 	})
 })
 
-describe('setter', () => {
+describe('Setter', () => {
 	let target
 
 	beforeEach(() => {
 		target = []
 	})
 
-	test('sets first level value', () => {
-		set(target, '0', { content: 'value', type: 'text' })
-		expect(target).toEqual([ { content: 'value', type: 'text' } ])
+	test('sets first level content', () => {
+		set(target, '0', 'content', 'value')
+		expect(target).toEqual([ { content: 'value' } ])
 	})
 
+	test('sets second level content', () => {
+		set(target, '0.0', 'content', 'value')
+		expect(target).toEqual([ { content: [ { content: 'value' } ] } ])
+	})
+
+	test('sets third level content', () => {
+		set(target, '0.0.0', 'content', 'value')
+		expect(target).toEqual([ { content: [ { content: [ { content: 'value' } ] } ] } ])
+	})
+
+	test('sets first level non first content', () => {
+		set(target, '3', 'content', 'value')
+		expect(target).toEqual([ undefined, undefined, undefined, { content: 'value' } ])
+	})
+
+	test('sets second level type', () => {
+		set(target, '0.0', 'type', 'link')
+		expect(target).toEqual([ { content: [ { type: 'link' } ] } ])
+	})
+
+	test('sets second level type in an already existing content', () => {
+		set(target, '0.0', 'content', 'value')
+		set(target, '0.0', 'type', 'link')
+		expect(target).toEqual([ { content: [ { type: 'link', content: 'value' } ] } ])
+	})
+
+	test('sets second level content in an already existing type', () => {
+		set(target, '0', 'type', 'bold')
+		set(target, '0.0', 'content', 'value')
+		expect(target).toEqual([ { type: 'bold', content: [ {  content: 'value' } ] } ])
+	})
+
+	test('sets second level type in an already existing nested content', () => {
+		set(target, '0.0.0', 'content', 'value')
+		set(target, '0.0', 'type', 'link')
+		expect(target).toEqual([ { content: [ { type: 'link', content: [ { content: 'value' } ] } ] } ])
+	})
+})
+
+describe('Navigate', () => {
+	test('navigate up in level 0', () => {
+		expect(goUp('0')).toBe('1')
+	})
+
+	test('navigate up in level 1', () => {
+		expect(goUp('0.1')).toBe('0.2')
+	})
+
+	test('navigate up in level 6', () => {
+		expect(goUp('0.1.2.3.1.2')).toBe('0.1.2.3.1.3')
+	})
+
+	test('navigate right in level 0', () => {
+		expect(goRight('0')).toBe('0.0')
+	})
+
+	test('navigate left in level 1', () => {
+		expect(goLeft('0.5')).toBe('0')
+	})
 })
 
 describe('Get tree',() => {
@@ -108,14 +167,42 @@ describe('Get tree',() => {
 		} ])
 	})
 
-	// test('nest a bold into a link', () => {
-	// 	const content = 'H24^B^bold link··'
-	// 	expect(getTree(content)).toEqual([ {
-	// 		type: 'link',
-	// 		content: [ {
-	// 			type: 'bold',
-	// 			content: 'bold link'
-	// 		} ]
-	// 	} ])
-	// })
+	test('nest a bold into a link', () => {
+		const content = 'H24^B^bold link··'
+		expect(getTree(content)).toEqual([ {
+			type: 'link',
+			content: [ {
+				type: 'bold',
+				content: 'bold link'
+			} ]
+		} ])
+	})
+
+	test('two bolds in a row', () => {
+		const content = 'B^bold one·B^bold two·'
+		expect(getTree(content)).toEqual([ {
+			type: 'bold',
+			content: 'bold one'
+		},{
+			type: 'bold',
+			content: 'bold two'
+		} ])
+	})
+
+	test('plain text, then bold, then link, then italics', () => {
+		const content = 'plainB^bold·H3^link·I^italic·'
+		expect(getTree(content)).toEqual([ {
+			type: 'text',
+			content: 'plain'
+		},{
+			type: 'bold',
+			content: 'bold'
+		},{
+			type: 'link',
+			content: 'link'
+		},{
+			type: 'italic',
+			content: 'italic'
+		} ])
+	})
 })
--- src/utils/textparser.js
+++ src/utils/textparser.js
@@ -25,8 +25,6 @@ const mix = text => {
 	}, []).filter(value => value && value !=='')
 }
 
-const getLast = array => array[array.length - 1]
-
 export const get = (arr, path) => {
 	return path.split('.').reduce((target, route) => {
 		if(!Array.isArray(target)) target = target.content
@@ -34,37 +32,65 @@ export const get = (arr, path) => {
 	}, arr)
 }
 
-export const set = (arr, path, value) => {
-	arr[path] = value
+export const set = (arr, path, field, value) => {
+	const target = path.split('.').reduce((trg, route, idx, arr) => {
+		const isLast = idx === arr.length - 1
+		if(!trg[route]) trg[route] = isLast ? {} : { content: [] }
+		
+		if(isLast){
+			return trg[route]
+		}else{
+			if(!trg[route].content) trg[route].content = []
+			return trg[route].content 
+		}
+	}, arr)
+	
+	target[field] = value
+
 	return arr
 }
 
-export const getTree = text => mix(text)
-	.reduce((acc, value) => {
-		const type = getType(value)
-		const lastElement = getLast(acc.content)
+export const goUp = path => {
+	const splitPath = path.split('.')
+	let change = +splitPath.pop() + 1
+	if(splitPath.length === 0) return `${change}`
+	return `${splitPath.join('.')}.${change}`
+}
 
-		if(type === 'text'){
-			lastElement.type = lastElement.type || 'text'
-			lastElement.content = value
-		}else if(type === 'close'){
-			acc.content.push({})
-		}else{
-			if(!lastElement.type){
-				lastElement.type = type
-			}else if( lastElement.type !== 'text') {
-				lastElement.content = [ {
-          
-				} ]
+export const goRight = path => `${path}.0`
+
+export const goLeft = path => {
+	if(path.length === 1) return path
+	const splitPath = path.split('.')
+	splitPath.pop()
+	return splitPath.join('.')
+}
+
+export const getTree = text => {
+	const abc = mix(text)
+	return abc
+		.reduce((acc, value) => {
+			let { content, pointer } = acc
+			const type = getType(value)
+
+			if(type === 'text'){
+				set(content, pointer, 'content', value )
+				if(!get(content, pointer).type)	set(content, pointer, 'type', 'text' )
+				pointer = goUp(pointer)
+			}else if(type === 'close'){
+				pointer = goLeft(pointer)
 			}else{
-				acc.content.push({
-					type
-				})
+				const current = get(content, pointer)
+				if(current && current.type){
+					pointer = goRight(pointer)
+				}
+				set(content, pointer, 'type', type)
 			}
-		}
-      
-		return acc
-	},{
-		content: [ {} ],
-		pointer: '0'
-	}).content.filter(obj => Object.keys(obj).length !== 0)
+
+			return { content, pointer }
+		},{
+			content: [ {} ],
+			pointer: '0'
+		}).content.filter(obj => Object.keys(obj).length !== 0)
+	
+}

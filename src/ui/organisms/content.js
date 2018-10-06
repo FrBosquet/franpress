ca9d39149926b032ce2ba6199a17ccf560eeae04
Fran Bosquet <fran@redradix.com>
2018-10-06 11:36:27 +0200

    Implement assets in generatod nodes

--- src/ui/organisms/content.js
+++ src/ui/organisms/content.js
@@ -6,21 +6,21 @@ import { ContentBodyParagraph, ContentBodyTitle, ContentBodyList, ContentBodySub
 import { ContentHeading, ContentMetadata, ContentBody } from '../molecules'
 import ContentBodyImage from '../atoms/content-body-image'
 
-const parseBranch = ({ type, content }, idx) => {
+const parseBranch = ({ type, content, asset }, idx) => {
 	switch(type){
 	case 'bold': return <strong key={idx}>{Array.isArray(content) ? content.map(parseBranch) : content}</strong>
-	case 'link': return <a key={idx} href='#' target='_blank' rel='noopener noreferrer'>{Array.isArray(content) ? content.map(parseBranch) : content}</a>
+	case 'link': return <a key={idx} href={asset} target='_blank' rel='noopener noreferrer'>{Array.isArray(content) ? content.map(parseBranch) : content}</a>
 	case 'italic': return <em key={idx}>{Array.isArray(content) ? content.map(parseBranch) : content}</em>
 	case 'text': return <Fragment key={idx}>{content}</Fragment>
 	}
 }
 
-const mapTextToNodes = content => getTree(content).map(parseBranch)
+const mapTextToNodes = (content, assets) => getTree(content, assets).map(parseBranch)
 
-const mapElementToNode = ({ type, content }, idx) => {
+const mapElementToNode = ({ type, content, assets }, idx) => {
 	switch(type){
 	case 'title':	return <ContentBodyTitle key={idx}>{content}</ContentBodyTitle>
-	case 'paragraph':	return <ContentBodyParagraph key={idx}>{mapTextToNodes(content)}</ContentBodyParagraph>
+	case 'paragraph':	return <ContentBodyParagraph key={idx}>{mapTextToNodes(content, assets)}</ContentBodyParagraph>
 	case 'list': return <ContentBodyList key={idx} content={content}/>
 	case 'subtitle': return <ContentBodySubtitle key={idx}>{content}</ContentBodySubtitle>
 	case 'image': return <ContentBodyImage key={idx} content={content}/>

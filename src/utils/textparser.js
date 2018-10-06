6dcf7c3efa96fe9c74f89d7ffa19f949f225f72f
Fran Bosquet <fran@redradix.com>
2018-10-06 10:43:47 +0200

    :art: Cleanup textparser

--- src/utils/textparser.js
+++ src/utils/textparser.js
@@ -1,3 +1,11 @@
+const CONTENT = 'content'
+const TYPE = 'type'
+const TEXT = 'text'
+const BOLD = 'bold'
+const LINK = 'link'
+const ITALIC = 'italic'
+const CLOSE = 'close'
+
 const close = /·/
 const splitRegex = /H\d+\^|I\^|B\^|·/g
 const boldRegex = /B\^/g
@@ -5,44 +13,51 @@ const linkRegex = /H\d+\^/
 const italicRegex = /I\^/
 
 const getType = text => {
-	if(text.search(boldRegex) === 0) return 'bold'
-	if(text.search(linkRegex) === 0) return 'link'
-	if(text.search(italicRegex) === 0) return 'italic'
-	if(text.search(close) === 0) return 'close'
-	return 'text'
+	if(text.search(boldRegex) === 0) return BOLD
+	if(text.search(linkRegex) === 0) return LINK
+	if(text.search(italicRegex) === 0) return ITALIC
+	if(text.search(close) === 0) return CLOSE
+	return TEXT
 }
 
-const isText = type => type === 'text'
-const isTag = type => [ 'bold','link','italic' ].includes(type)
+const isLast = (arr, idx) => idx === arr.length - 1
+const isObject = target => typeof target !== 'object'
+const isEmptyObject = obj => Object.keys(obj).length === 0
+const isNotEmptyObject = obj => !isEmptyObject(obj)
 
-const mix = text => {
-	const delimiters = text.match(splitRegex) || []
-	const contents = text.split(splitRegex)
+const extractTags = text => text.match(splitRegex) || []
+const extractContent = text => text.split(splitRegex)
+
+const merge = text => {
+	const tags = extractTags(text)
+	const contents = extractContent(text)
 	
-	return contents.reduce((chain, content, idx) => {
-		chain.push(content)
-		chain.push(delimiters[idx])
-		return chain
-	}, []).filter(value => value && value !=='')
+	return contents
+		.reduce((chain, content, idx) => [
+			...chain,
+			content,
+			tags[idx] ]
+			, [])
+		.filter(value => value && value !=='')
 }
 
-export const get = (arr, path) => {
-	return path.split('.').reduce((target, route) => {
+export const get = (arr, path) => 
+	path.split('.').reduce((target, route) => {
 		if(!Array.isArray(target)) target = target.content
 		return target[route]
 	}, arr)
-}
 
 export const set = (arr, path, field, value) => {
 	const target = path.split('.').reduce((trg, route, idx, arr) => {
-		const isLast = idx === arr.length - 1
-		if(!trg[route]) trg[route] = isLast ? {} : { content: [] }
+		const lastElement = isLast(arr, idx)
 		
-		if(isLast){
+		if(!trg[route]) trg[route] = lastElement ? {} : { content: [] }
+		
+		if(lastElement){
 			return trg[route]
 		}else{
 			if(!trg[route].content) trg[route].content = []
-			if(typeof trg[route].content !== 'object') trg[route].content = []
+			if(isObject(trg[route].content)) trg[route].content = []
 			return trg[route].content 
 		}
 	}, arr)
@@ -52,6 +67,8 @@ export const set = (arr, path, field, value) => {
 	return arr
 }
 
+export const goRight = path => `${path}.0`
+export const goLeft = path => path.slice(0, path.length - 2)
 export const goUp = path => {
 	const splitPath = path.split('.')
 	let change = +splitPath.pop() + 1
@@ -59,55 +76,50 @@ export const goUp = path => {
 	return `${splitPath.join('.')}.${change}`
 }
 
-export const goRight = path => `${path}.0`
+const mutateTree = (tree, mutator) => {
+	let { content, pointer } = tree
+	const type = getType(mutator)
+	const current = get(content, pointer)
 
-export const goLeft = path => {
-	const splitPath = path.split('.')
-	splitPath.pop()
-	return splitPath.join('.')
-}
+	switch(type){
+	case TEXT:
+		set(content, pointer, CONTENT, mutator )
+		if(!get(content, pointer).type)	set(content, pointer, TYPE, TEXT )
+		break
+	case CLOSE:
+		pointer = current ?
+			goUp(pointer) :
+			goUp(goLeft(pointer))
+		break
+	case BOLD:
+	case ITALIC:
+	case LINK:
+	default:
+		if(current && current.type){
+			switch(current.type){
+			case TEXT:
+				pointer = goUp(pointer)
+				break
+			case BOLD:
+			case ITALIC:
+			case LINK:
+			default:
+				pointer = goRight(pointer)
 
-export const getTree = text => {
-	return mix(text)
-		.reduce((acc, value) => {
-			let { content, pointer } = acc
-			const type = getType(value)
-			const current = get(content, pointer)
-
-			if(type === 'text'){
-				set(content, pointer, 'content', value )
-				if(!get(content, pointer).type)	set(content, pointer, 'type', 'text' )
-
-			}else if(type === 'close'){
-				if(current){
+				if(current.content) {
+					set(content, pointer, CONTENT, current.content )
+					set(content, pointer, TYPE, TEXT )
 					pointer = goUp(pointer)
-				}else{
-					pointer = goUp(goLeft(pointer))
 				}
-
-			}else{
-				if(current){
-
-					if(current.type && isTag(current.type)){
-						pointer = goRight(pointer)
-
-						if(current.content) {
-							set(content, pointer, 'content', current.content )
-							set(content, pointer, 'type', 'text' )
-							pointer = goUp(pointer)
-						}
-					}else if(current.type && isText(current.type)){
-						pointer = goUp(pointer)
-					}	
-				}
-
-				set(content, pointer, 'type', type )
 			}
-
-			return { content, pointer }
-		},{
-			content: [ {} ],
-			pointer: '0'
-		}).content.filter(obj => Object.keys(obj).length !== 0)
-	
+		}
+		set(content, pointer, TYPE, type )
+	}
+	return { content, pointer }
 }
+
+export const getTree = text => 
+	merge(text)
+		.reduce(mutateTree, { content: [ {} ],	pointer: '0' })
+		.content
+		.filter(isNotEmptyObject)

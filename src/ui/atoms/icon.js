e9c38eadcbb60d4105d8eb1351c12ab7b02c1a25
Fran Bosquet <fran@redradix.com>
2018-10-07 12:17:43 +0200

    :art::construction: Pass fill color to svg using css and not a prop, and animate it

--- src/ui/atoms/icon.js
+++ src/ui/atoms/icon.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Icons from './icons'
+
+const SVG = styled.svg`
+	fill: ${props => props.color};
+	isolation: isolate;
+	margin: ${props => props.margin}px;
+	transform: ${props => props.flip ? 'rotate(180deg)' : ''};
+
+	transition: all 0.4s ease-in;
+`
+
+SVG.defaultProps = {
+	color: '#931A38'
+}
 
 const Icon = ({ width, margin, color, type, flip }) => {
 	const DesiredIcon = Icons[type] || Icons['logo']
 	return (
-		<svg style={{ isolation: 'isolate', margin, transform: flip ? 'rotate(180deg)' : '' }} viewBox="0 0 100 100" width={ width } height={ width }>
+		<SVG color={color} margin={margin} flip={flip} viewBox="0 0 100 100" width={ width } height={ width }>
 			<DesiredIcon color={ color }/>
-		</svg>
+		</SVG>
 	)
 }
 
--- src/ui/atoms/icons/blog-logo.js
+++ src/ui/atoms/icons/blog-logo.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-const Logo = ({ color }) => 
+const Logo = () => 
 	(
 		<Fragment>
 			<defs>
@@ -12,18 +12,12 @@ const Logo = ({ color }) =>
 			<g clipPath="url(#_clipPath_JUKwV52oTaUH8ERMxsRmMq2LzgDYKCqd)">
 				<rect width="100" height="100" fillOpacity="0"/>
 				<g>
-					<path d=" M 10 100 L 73.04 60.6 C 82.4 54.75 81.038 50 70 50 L 60 50 L 75.858 34.142 C 83.663 26.337 81.038 20 70 20 L 20 20 L 20 50 L 60 50 L 0 100 L 0 0 L 85 0 C 93.279 0 100 6.721 100 15 L 100 85 C 100 93.279 93.279 100 85 100 L 0 100 L 10 100 Z " fill={ color }/>
+					<path d=" M 10 100 L 73.04 60.6 C 82.4 54.75 81.038 50 70 50 L 60 50 L 75.858 34.142 C 83.663 26.337 81.038 20 70 20 L 20 20 L 20 50 L 60 50 L 0 100 L 0 0 L 85 0 C 93.279 0 100 6.721 100 15 L 100 85 C 100 93.279 93.279 100 85 100 L 0 100 L 10 100 Z "/>
 				</g>
 			</g>
 		</Fragment>
 	)
 
-Logo.propTypes = {
-	color: PropTypes.string
-}
-  
-Logo.defaultProps = {
-	color: '#931A38'
-}
+
   
 export default Logo

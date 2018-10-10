089e2d3319908324381a5259f759288dcbae16e0
Fran Bosquet <fran@redradix.com>
2018-10-10 18:52:16 +0200

    (Server) Add search post by ID

--- server/index.js
+++ server/index.js
@@ -4,18 +4,21 @@ const posts = require('./posts.json')
 
 const typeDefs = gql`
   type Post {
+    id: ID!
     title: String
     content: String
   }
 
   type Query {
     posts: [Post]
+    post(id: ID!): Post
   }
 `
 
 const resolvers = {
 	Query: {
-		posts: () => posts
+		posts: () => posts,
+		post: (root, args) => posts.find(post => post.id === args.id)
 	}
 }
 

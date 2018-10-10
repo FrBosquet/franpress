0fc9b8b6e7379b34d6df51baf66c9f4ff04f6cdb
Fran Bosquet <fran@redradix.com>
2018-10-10 19:06:15 +0200

    (server) Add search by urlified title

--- server/index.js
+++ server/index.js
@@ -3,22 +3,38 @@ const { ApolloServer, gql } = require('apollo-server')
 const posts = require('./posts.json')
 
 const typeDefs = gql`
+  type Content {
+    type: String
+    content: String
+    assets: [String]
+  }
+
   type Post {
     id: ID!
     title: String
-    content: String
+    subtitle: String
+    photoAuthor: String
+    date: String
+    tags: [String]
+    content: [Content]
   }
 
   type Query {
     posts: [Post]
-    post(id: ID!): Post
+    post(id: ID, url: String): Post
   }
 `
 
 const resolvers = {
 	Query: {
 		posts: () => posts,
-		post: (root, args) => posts.find(post => post.id === args.id)
+		post: (root, args) => {
+			if(args.id) return posts.find(post => post.id === args.id)
+			if(args.url) return posts.find(post => {
+				console.log(post.title.toLowerCase().replace(' ', '-'))
+				return post.title.toLowerCase().replace(' ', '-') === args.url
+			})
+		}
 	}
 }
 

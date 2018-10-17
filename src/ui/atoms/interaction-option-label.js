ad6254b48e7508c1e518207743b38605531c2b00
Fran Bosquet <fran@redradix.com>
2018-10-17 22:06:09 +0200

    Add interaction button disabling

--- src/ui/atoms/interaction-option-label.js
+++ src/ui/atoms/interaction-option-label.js
@@ -2,12 +2,13 @@ import styled from 'styled-components'
 
 const InteractionOptionLabel = styled.span`
   font-family: 'Barlow', sans-serif;
-  color: #4B0013;
+  color: ${ props => props.disabled ? '#d5c4c7' : '#4B0013'};
   font-size: 14px;
   font-weight: 700;
   margin: 5px 0px;
   text-transform: uppercase;
-  margin-left: ${props => props.gap}
+  margin-left: ${props => props.gap};
+  user-select: none;
   `
 
 export default InteractionOptionLabel
--- src/ui/molecules/interaction-option.js
+++ src/ui/molecules/interaction-option.js
@@ -6,25 +6,27 @@ import InteractionOptionLabel from '../atoms/interaction-option-label'
 
 const Anchor = styled.a`
   align-items: center;
-  cursor: pointer;
+  cursor: ${ props => props.disabled ? 'auto' : 'pointer'};
   display: flex;
 	margin-right: ${props => props.gap};
 `
 
-const InteractionOption = ({ type, label, flip }) => 
-	<Anchor target='_blank' >
-		<Icon type={ type } width={32} margin={5} flip={flip}/>
-		<InteractionOptionLabel gap={10}>{label}</InteractionOptionLabel>
+const InteractionOption = ({ type, label, flip, onClick, disabled }) => 
+	<Anchor onClick={onClick} target='_blank' disabled={disabled}>
+		<Icon color={disabled ? '#d5c4c7' : '#4B0013'} type={ type } width={32} margin={5} flip={flip} disabled={disabled}/>
+		<InteractionOptionLabel disabled={disabled} gap={10}>{label}</InteractionOptionLabel>
 	</Anchor>
 
 InteractionOption.propTypes = {
+	onClick: PropTypes.func,
 	type: PropTypes.string,
 	url: PropTypes.string,
 	width: PropTypes.number,
 	margin: PropTypes.number,
 	gap: PropTypes.number,
 	flip: PropTypes.bool,
-	label: PropTypes.string
+	label: PropTypes.string,
+	disabled: PropTypes.bool
 }
 
 InteractionOption.defaultProps = {
